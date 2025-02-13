32fe236151bf44abe9660c726e6b823b053bc0e0
elian-sss <elian.silva.if@gmail.com>
2025-02-13 16:54:04 -0300

    adiciona novo elemento

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adiciona(1);
+
+        System.out.println(lista);
+    }
+}
